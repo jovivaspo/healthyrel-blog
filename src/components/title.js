ccf0b26007fc9bda935caf32c1c1100145493693
jovivaspo <jovivaspo@gmail.com>
2023-01-14 17:15:57 +0100

    Updates

--- src/components/title.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Title = ({ title }) => {
-  return <h1 className="title">{title}</h1>;
-};
-
-export default Title;
--- src/services/get-date.js
+++
@@ -1,6 +0,0 @@
-const getDate = (date) => {
-  const dateParse = new Date(date).toLocaleDateString();
-  return dateParse;
-};
-
-export default getDate;
